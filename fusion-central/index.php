052c1866704f11c1f34d1eeca1c175a220ca6e07
Pablo Sánchez <71054958+pasabaa@users.noreply.github.com>
2021-08-06 15:32:01 -0500

    Upload Project
    Archivos necearios

---
+++ fusion-central/index.php
@@ -0,0 +1,12 @@
+<?php include('template/header.php') ?>
+
+<div class="container-fluid vh-100">
+    <div class="row justify-content-center align-items-center vh-100">
+        <div class="col-md-5 col-12">
+            <div class="text-center">
+                <h1 class="display-4 fw-bolder">Bienvenidos a <span class="text-green">Fusion Central</span></h1>
+                <img class="img-fluid" src="img/products/code.svg" alt="" width="464">
+            </div>
+        </div>
+    </div>
+</div>
---
+++ fusion-central/nosotros.php
@@ -0,0 +1,67 @@
+<?php include('template/header.php') ?>
+
+<div class="container">
+    <div class="row p-4">
+        <div class="col-md-8 col-12">
+            <h1 class="display-4 fw-bolder">Nosotros <span class="text-green">Fusion Central</span></h1>
+        </div>
+    </div>
+
+    <div class="row p-4 justify-content-center align-items-center">
+        <div class="col-md-5 col-12">
+            <div class="card bg-rich-secondary border-0 p-4">
+                <h1 class="fw-bolder">Vision</h1>
+                <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo totam tempora quas neque
+                    sapiente iusto
+                    error ipsam</p>
+            </div>
+        </div>
+        <div class="col-md-4 col-12">
+            <div class="p-4">
+                <div class="text-center">
+                    <img class="img-fluid" src="img/us/vision.svg" alt="" width="334">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row p-4 justify-content-center">
+        <div class="col-md-4 col-12">
+            <div class="p-4">
+                <div class="text-center">
+                    <img class="img-fluid" src="img/us/target.svg" alt="" width="334">
+                </div>
+            </div>
+        </div>
+        <div class="col-md-5 col-12">
+            <div class="card bg-rich-secondary border-0 p-4">
+                <h1 class="fw-bolder">Misión</h1>
+                <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo totam tempora quas neque
+                    sapiente iusto
+                    error ipsam</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="row p-4 justify-content-center">
+        <div class="col-md-5 col-12">
+            <div class="card bg-rich-secondary border-0 p-4">
+                <h1 class="fw-bolder">Valores</h1>
+                <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo totam tempora quas neque
+                    sapiente iusto
+                    error ipsam</p>
+            </div>
+        </div>
+        <div class="col-md-4 col-12">
+            <div class="p-4">
+                <div class="text-center">
+                    <img class="img-fluid" src="img/us/val.svg" alt="" width="334">
+                </div>
+            </div>  
+        </div>
+    </div>
+
+</div>
+
+
+<?php include('template/footer.php') ?>
